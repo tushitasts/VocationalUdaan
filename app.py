123e9ca418d19e906f1c2d29eda1274b51a8129d
tushitasts <tushitasts@gmail.com>
2025-12-20 18:09:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,12 @@ db_uri = os.getenv(
 if db_uri.startswith("postgres://"):
     db_uri = db_uri.replace("postgres://", "postgresql://", 1)
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "pool_pre_ping": True,
+    "pool_recycle": 300
+}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # IMPORTANT: initialize the imported db with the app (do NOT create a new SQLAlchemy instance)
@@ -342,6 +347,7 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
 
 
 
